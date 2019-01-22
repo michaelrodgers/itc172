3780b4a98e7f3487904fbd87d2e43758ce17056e
Michael Rodgers <38140530+michaelrodgers@users.noreply.github.com>
2019-01-22 14:17:36 -0800

    Add files via upload

--- PythonClub/club/admin.py
+++ PythonClub/club/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Event, Meeting, Minutes, Resource
 
 # Register your models here.
+admin.site.register(Event)
+admin.site.register(Meeting)
+admin.site.register(Minutes)
+admin.site.register(Resource)
--- PythonClub/club/models.py
+++ PythonClub/club/models.py
@@ -3,25 +3,54 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class ClubType(models.Model):
-    typename = models.CharField(max_length=255)
-    typedescription = models.CharField(max_length=255)
+class Meeting(models.Model):
+    meetingtitle = models.CharField(max_length=255)
+    meetingdate = models.DateField()
+    meetingtime = models.TimeField()
+    meetinglocation = models.CharField(max_length=255, null=True, blank=True)
+    agenda = models.TextField(null=True, blank=True)
 
     def __str__(self):
-        return self.typename
+        return self.meetingtitle
 
     class Meta:
-        db_table = 'clubtype'
+        db_table = 'meeting'
 
-class ClubProduct(models.Model):
-    productname = models.CharField(max_length=255)
-    clubtype = models.ForeignKey(ClubType, on_delete=models.CASCADE)
+class Minutes(models.Model):
+    meetingid = models.ForeignKey(Meeting, on_delete=models.DO_NOTHING)
+    attendance = models.ManyToManyField(User)
+    minutetext = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.meetingid
+
+    class Meta:
+        db_table = 'minutes'
+
+class Resource(models.Model):
+    resourcename = models.CharField(max_length=255)
+    resourcetype = models.CharField(max_length=255)
+    resourceURL = models.URLField(null=True, blank=True)
     entryDate = models.DateField()
-    productURL = models.URLField()
-    productdescription = models.TextField()
+    userid = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    resourcedescription = models.TextField(null=True, blank=True)
 
     def __str__(self):
-        return self.productname
+        return self.resourcename
 
     class Meta:
-        db_table='clubproduct'
+        db_table = 'resource'
+
+class Event(models.Model):
+    eventtitle = models.CharField(max_length=255)
+    eventlocation = models.CharField(max_length=255, null=True, blank=True)
+    eventdate = models.DateField()
+    eventtime = models.TimeField()
+    eventdescription = models.TextField(null=True, blank=True)
+    userid = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.eventtitle
+
+    class Meta:
+        db_table = 'event'
